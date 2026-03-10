793c2dc1a15cbca0a5f0aaa47eb880cbfea08202
woojin1254-stack <woojin1254@gmail.com>
2026-03-10 17:49:26 +0900

    Fix final git conflict in index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,3 @@
-<<<<<<< HEAD
-// This is your entry file! Refer to it when you render:
-// npx remotion render <entry-file> HelloWorld out/video.mp4
-
-=======
->>>>>>> origin/main
 import { registerRoot } from "remotion";
 import { RemotionRoot } from "./Root";
 
